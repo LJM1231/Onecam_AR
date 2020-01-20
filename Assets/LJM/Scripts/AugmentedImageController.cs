ebffc2d8ec106deac13c5ab63b51154b14ae431c
jimin1231 <polly12312@naver.com>
2020-01-21 00:10:41 +0900

    02
    2020.01.21

---
+++ Assets/LJM/Scripts/AugmentedImageController.cs
@@ -0,0 +1,42 @@
+﻿using GoogleARCore;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AugmentedImageController : MonoBehaviour
+{
+    [SerializeField] private AugmentedImageVisualizer _augmentedImageVisualizer;
+
+    private readonly Dictionary<int, AugmentedImageVisualizer> _visualizers =
+        new Dictionary<int, AugmentedImageVisualizer>();
+
+    private readonly List<AugmentedImage> _images = new List<AugmentedImage>();
+
+
+    private void Update()
+    {
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            return;
+        }
+
+        Session.GetTrackables(_images, TrackableQueryFilter.Updated);
+        VisualizeTrackables();
+    }
+
+    private void VisualizeTrackables()
+    {
+        foreach (var image in _images)
+        {
+            var visualizer = GetVisualalizer(image);
+        }
+    }
+
+    private AugmentedImageVisualizer GetVisualalizer(AugmentedImage image)
+    {
+        AugmentedImageVisualizer visualizer;
+        _visualizers.TryGetValue(image.DatabaseIndex, out visualizer);
+        return visualizer;
+    }
+}
